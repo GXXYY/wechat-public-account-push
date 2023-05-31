f386da7de85caa3f1143875a425f93db5376c0b4
GXXYY <135047110+GXXYY@users.noreply.github.com>
2023-05-31 13:44:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oO9Oe6V96_iEHMiRGGICLJdcp0q0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'h75UjKqN71SNFfCJSyVHE9MMXjrcYaqYdyL-aJ65cEk',
+      useTemplateId: 'IElfGckGEvW1qg4IBuWMAYJ-hQ8BP-oOmMvKbkIpEc0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-03',
       festivals: [
@@ -45,7 +45,7 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2023-04-15' },
         // 相识纪念日
-        { keyword: 'know_day', date: '2022-09-09' },
+        { keyword: 'know_day', date: '2022-02-17' },
       ],
     },
   ],
