1771af17c2ff34bcfde5e39d2a4ee3dcccbb1feb
GXXYY <135047110+GXXYY@users.noreply.github.com>
2023-05-31 16:20:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oO9Oe6V96_iEHMiRGGICLJdcp0q0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'o0k8Z6l1wR9eaJF0dpvC4IKcnUXnaDAF-d3yqSKnunc',
+      useTemplateId: '	JPEApJliE7G5OqTsQdi5UD22-mYIOJAZXAg87YuxBqI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-03',
       horoscopeDateType: '今日',
@@ -60,7 +60,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oO9Oe6V96_iEHMiRGGICLJdcp0q0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'o0k8Z6l1wR9eaJF0dpvC4IKcnUXnaDAF-d3yqSKnunc',
+      useTemplateId: '1I5YeM013YljW80elXP3mzlAno6f37XUfTbZKESzbCQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-31',
       horoscopeDateType: '今日',
@@ -89,7 +89,79 @@ const USER_CONFIG = {
         // 相识纪念日
         { keyword: 'know_day', date: '2022-02-17' },
       ],
-    },   
+    }, 
+    {
+      // 想要发送的人的名字
+      name: '可爱瑶瑶',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oO9Oe6fYa08bR4hXbcPlJxtL_0Ks',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '	JPEApJliE7G5OqTsQdi5UD22-mYIOJAZXAg87YuxBqI',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '10-03',
+      horoscopeDateType: '今日',
+      horoscope: true,
+      birthdayMessage: true,
+      holidaytts: true,
+      earthyLoveWords: true,
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '瑶瑶', year: '2002', date: '09-08',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '瑶瑶', year: '2002', date: '10-13',
+        },
+        {
+          type: '*节日', name: '相识纪念日', year: '2022', date: '02-17',
+        },
+      ],
+      FESTIVALS_LIMIT: 0,
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-04-15' },
+        // 相识纪念日
+        { keyword: 'know_day', date: '2022-02-17' },
+      ],
+    },
+   {
+      // 想要发送的人的名字
+      name: '小管',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oO9Oe6fYa08bR4hXbcPlJxtL_0Ks',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '1I5YeM013YljW80elXP3mzlAno6f37XUfTbZKESzbCQ',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '03-31',
+      horoscopeDateType: '今日',
+      horoscope: true,
+      birthdayMessage: true,
+      holidaytts: true,
+      earthyLoveWords: true,
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '小管', year: '2003', date: '02-29',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '小管', year: '2003', date: '03-31',
+        },
+        {
+          type: '*节日', name: '相识纪念日', year: '2022', date: '02-17',
+        },
+      ],
+      FESTIVALS_LIMIT: 0,
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-04-15' },
+        // 相识纪念日
+        { keyword: 'know_day', date: '2022-02-17' },
+      ],
+    },
   ],
   
   
