59b510594c5bec50a124f4d0be59db198e13cf9d
GXXYY <135047110+GXXYY@users.noreply.github.com>
2023-05-31 15:41:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -16,12 +16,13 @@ const USER_CONFIG = {
   PROVINCE: '江西',
   CITY: '上饶',
 
+  IS_SHOW_COLOR: false,
   USERS: [
     {
       // 想要发送的人的名字
-      name: '瑶瑶',
+      name: '可爱瑶瑶',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oO9Oe6V96_iEHMiRGGICLJdcp0q0',
+      id: 'oO9Oe6V96_iEHMiRGGICLJdcp0q0','oO9Oe6fYa08bR4hXbcPlJxtL_0Ks'
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'eD2eFwMbwqm3FKl-oC5yvmht3W2dCoyewaDRCHjfMLc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -38,10 +39,10 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '瑶瑶', year: '2002', date: '10-03',
+          type: '生日', name: '瑶瑶', year: '2002', date: '10-13',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '*节日', name: '相识纪念日', year: '2022', date: '02-17',
         },
       ],
       FESTIVALS_LIMIT: 0,
@@ -55,7 +56,46 @@ const USER_CONFIG = {
     },
   ],
 
-
+ USERS: [
+    {
+      // 想要发送的人的名字
+      name: '小管',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oO9Oe6V96_iEHMiRGGICLJdcp0q0','oO9Oe6fYa08bR4hXbcPlJxtL_0Ks'
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'eD2eFwMbwqm3FKl-oC5yvmht3W2dCoyewaDRCHjfMLc',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '03-31',
+      horoscopeDateType: '今日',
+      horoscope: true,
+      birthdayMessage: true,
+      holidaytts: true,
+      earthyLoveWords: true,
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '小管', year: '2003', date: '02-29',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '小管', year: '2003', date: '03-31',
+        },
+        {
+          type: '*节日', name: '相识纪念日', year: '2022', date: '02-17',
+        },
+      ],
+      FESTIVALS_LIMIT: 0,
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-04-15' },
+        // 相识纪念日
+        { keyword: 'know_day', date: '2022-02-17' },
+      ],
+    },
+  ],
+  
+  
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: '6Tz15_XncaEtz3sWlPGTd_x7qBLtD1ReZtFxF92HX-8',
 
